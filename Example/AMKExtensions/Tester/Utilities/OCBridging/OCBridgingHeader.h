28b2f93dca1b9c814b3a40f62329465337725aaa
mengxinxin <mengxinxin@baidu.com>
2022-07-08 09:45:30 +0800

    feat(Tester): 添加 OC 桥接头文件

---
+++ Example/AMKExtensions/Tester/Utilities/OCBridging/OCBridgingHeader.h
@@ -0,0 +1,17 @@
+//
+//  OCBridgingHeader.h
+//  AMKExtensions
+//
+//  Created by 孟昕欣 on 2022/7/8.
+//  Copyright © 2022 https://github.com/AndyM129. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OCBridgingHeader : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/AMKExtensions/Tester/Utilities/OCBridging/OCBridgingHeader.m
@@ -0,0 +1,13 @@
+//
+//  OCBridgingHeader.m
+//  AMKExtensions
+//
+//  Created by 孟昕欣 on 2022/7/8.
+//  Copyright © 2022 https://github.com/AndyM129. All rights reserved.
+//
+
+#import "OCBridgingHeader.h"
+
+@implementation OCBridgingHeader
+
+@end
